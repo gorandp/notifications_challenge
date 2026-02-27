6074a285f1ef77ad43287759d4f311610feb494b
Goran Prpic <gorandp.dev@gmail.com>
2026-02-27 15:46:53 -0300

    add auth tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,3 +1,37 @@
 # Register
 # Login
 # Authorization in endpoints
+from src.app.fastapi_app.database import db_session
+from src.app import database_models as models
+from src.app.config import JWTConfig
+import hashlib
+
+
+def test_unauthenticated(client):
+    r = client.get("/testAuth")
+    assert r.status_code == 401
+
+
+def test_login(client):
+    JWTConfig.set_secret("secret123")
+    db = db_session.get()
+    password_hash = hashlib.sha256("password123".encode('utf-8')).hexdigest()
+    new_user = models.User(
+        email="test@example.com",
+        password_hash=password_hash,
+        enabled=True
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    r = client.post(
+        "/login",
+        json={
+            "email": "test@example.com",
+            "password": "password123"
+        }
+    )
+
+    assert r.status_code == 200
+    assert "access_token" in r.json()
