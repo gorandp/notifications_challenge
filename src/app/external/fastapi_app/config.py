69445db19d0deb9c62242469ab1fc2318e2ad28a
Goran Prpic <gorandp@outlook.com>
2026-04-07 15:23:31 -0300

    jwt config update

--- src/app/external/fastapi_app/config.py
+++ src/app/external/fastapi_app/config.py
@@ -1,8 +1,12 @@
-
 class JWTConfig:
     SECRET = ""
     ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
     @classmethod
     def set_secret(cls, secret: str):
         cls.SECRET = secret
+
+    @classmethod
+    def set_expire_minutes(cls, expire_minutes: int):
+        cls.ACCESS_TOKEN_EXPIRE_MINUTES = expire_minutes
