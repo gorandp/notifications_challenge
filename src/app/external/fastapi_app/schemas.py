8cb429fa37af4259e21f4eb6261f8354e7659b36
Goran Prpic <gorandp@outlook.com>
2026-04-07 16:05:54 -0300

    fix

--- src/app/external/fastapi_app/schemas.py
+++ src/app/external/fastapi_app/schemas.py
@@ -7,25 +7,29 @@ class AuthResponse(BaseModel):
     access_token: str
     token_type: str
 
+
 class AuthRequest(BaseModel):
-    email: str = Field(max_length=120)
+    username: str = Field(max_length=120)
     password: str = Field(max_length=256)
+
 
 class AuthTestResponse(BaseModel):
     current_user_id: int = Field()
     success: bool = Field(default=False)
 
+
 class UserBase(BaseModel):
     email: str = Field(max_length=120)
     enabled: bool = Field(default=False)
+
 
 class UserResponse(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
 
+
 class UserCreate(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
     password: str = Field(max_length=256)
-
