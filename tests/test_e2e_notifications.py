899ce6f38b2501fb0962ed7a87e77f33d5f60ed1
Goran Prpic <gorandp@outlook.com>
2026-04-21 11:00:39 -0300

    feat: add tests for notification retrieval with authentication

--- tests/test_e2e_notifications.py
+++ tests/test_e2e_notifications.py
@@ -2,10 +2,35 @@
 # Edit
 # Delete
 # Query all
+from fastapi import status
 
-def test_add_notification(client):
-    r = client.get("/")
-    print(r)
-    assert r.status_code == 200
-    assert r.json() == {"msg": "Hello World!"}
+from db_data import generate_user, generate_notification
+from auth import login
 
+
+def test_auth_get_notification(client):
+    r = client.get("/notifications")
+    assert r.status_code == status.HTTP_401_UNAUTHORIZED
+
+    user, pwd = generate_user()
+    token = login(client, user.email, pwd)
+    r = client.get(
+        "/notifications",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert r.status_code == status.HTTP_200_OK
+    assert r.json() == []
+
+
+def test_get_notifications(client):
+    user, pwd = generate_user()
+    notification = generate_notification(user.id)
+    token = login(client, user.email, pwd)
+
+    r = client.get(
+        "/notifications",
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    assert r.status_code == status.HTTP_200_OK
+    data = r.json()
+    assert data[0]["id"] == notification.id
