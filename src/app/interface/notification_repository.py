06bd244b6efd4187bc8ca77d4c6a55c0718cfb03
Goran Prpic <gorandp@outlook.com>
2026-05-01 20:07:41 -0300

    feat: update notification timestamps in update and update_status methods

--- src/app/interface/notification_repository.py
+++ src/app/interface/notification_repository.py
@@ -19,13 +19,15 @@ class NotificationRepository(INotificationRepository):
         return await self.db.get_all_notifications_by_user_id(user_id)
 
     async def update(self, notification_id, notification):
+        notification.updated_at = datetime.now(UTC)
         return await self.db.update_notification(notification_id, notification)
 
     async def update_status(self, notification_id, notification_status):
         n = await self.get_by_id(notification_id)
         n.status = notification_status
-        # if notification_status == NotifStatus.SENT.value:
-        #     n.sent_at = datetime.now(UTC)
+        n.updated_at = datetime.now(UTC)
+        if notification_status == NotifStatus.SENT.value:
+            n.sent_at = datetime.now(UTC)
         return await self.db.update_notification(notification_id, n)
 
     async def delete(self, notification_id):
