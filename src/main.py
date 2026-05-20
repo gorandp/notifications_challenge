14277038e84c42b53a542c0dd5ca7a3f9f8bec25
Goran Prpic <gorandp@outlook.com>
2026-05-20 12:58:07 -0300

    add middleware for database session management

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 import os
 
+from fastapi import Request
 from app.external.fastapi_app.main import app as app
 
 from app.core.logger import LoggerConfig
 from app.external.database.database import Database
-from app.external.fastapi_app.context import init_context
+from app.external.fastapi_app.context import (
+    init_context,
+    db_session,
+    database_ctx,
+)
 from app.external.database.database_models import Base as DatabaseBaseModel
 from app.external.fastapi_app.config import JWTConfig
 
@@ -15,3 +20,13 @@ database = Database({"url": os.getenv("DB_CONNECTION_STRING", "sqlite:///./dev.d
 # Create tables if not exist
 DatabaseBaseModel.metadata.create_all(bind=database.engine)
 init_context(database)
+
+
+@app.middleware("http")
+async def set_db_session(request: Request, call_next):
+    db = database_ctx.get()
+    with db.session_local() as session:
+        token = db_session.set(session)
+        response = await call_next(request)
+        db_session.reset(token)
+    return response
