df5474048ef9edd5ddbfc570788b82986763af0f
Goran Prpic <gorandp@outlook.com>
2026-08-01 08:58:23 -0300

    config alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,9 +7,14 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from src.app.external.database import database_models
+from src.app.external.fastapi_app.main import env_settings
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", env_settings.DB_CONNECTION_STRING)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = database_models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
