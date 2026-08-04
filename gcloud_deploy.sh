dad5599c71bd44aeeee5fe503018cae01500f23b
Goran Prpic <gorandp@outlook.com>
2026-08-04 18:34:10 -0300

    update gcloud deploy script

--- gcloud_deploy.sh
+++ gcloud_deploy.sh
@@ -2,14 +2,7 @@
 gcloud builds submit \
    --tag us-east4-docker.pkg.dev/notifications-challenge/notif-challenge-repo/notif-challenge-api:latest
 
-# Deploy
-# gcloud run deploy notif-challenge-service \
-#    --image us-east4-docker.pkg.dev/notifications-challenge/notif-challenge-repo/notif-challenge-api:latest \
-#    --region us-east4 \
-#    --allow-unauthenticated \
-#    --command /bin/sh \
-#    --args '-c,exec fastapi run --host 0.0.0.0 --port "${PORT:-8000}" src/main.py --proxy-headers --forwarded-allow-ips "*"'
-# Deploy simple (TODO: check if it works)
+# Deploy simple
 gcloud run deploy notif-challenge-service \
    --image us-east4-docker.pkg.dev/notifications-challenge/notif-challenge-repo/notif-challenge-api:latest \
    --region us-east4
